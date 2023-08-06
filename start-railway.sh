ce9389ff7bfa549a3bbafc2b6e831c2e89ea4074
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-08-06 11:58:05 +0200

    [SH] Improve start script

--- start-railway.sh
+++ start-railway.sh
@@ -4,15 +4,23 @@ create_admin_user() {
   output=$(./manage.py registeradmin "$ADMIN_EMAIL" 2>&1) 
 
   if [ $? -eq 1 ]; then  
+      echo "$output"
       echo "Error occured, user might already exist"    
   else
       echo "$output"  
   fi
 }
+
+if [[ -v ADMIN_EMAIL ]]; then
+    echo "ADMIN_EMAIL is defined. So creating admin user."
+    set -m
   
-set -m
+    ./entrypoint.sh &
+      create_admin_user
+      
+    fg %1
+
+else
+    echo "ADMIN_EMAIL is not defined. So not creating admin user"
+fi
   
-./entrypoint.sh &
-  create_admin_user
-  
-fg %1
